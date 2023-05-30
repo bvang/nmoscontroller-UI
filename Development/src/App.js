b4c1b7a5b8050a575cd0cc2c8d6618e89f6778f9
bvang <billy.vang27@gmail.com>
2023-05-30 18:44:14 +0200

    code allégé 3

--- Development/src/App.js
+++ Development/src/App.js
@@ -60,8 +60,7 @@ const AppAdmin = () => (
             show={ReceiversShow}
             edit={ReceiversEdit}
         />
-        <Resource name="routing" list={RoutingPage}
-        />
+        <Resource name="routing" list={RoutingPage} />
         <Resource
             name="subscriptions"
             list={SubscriptionsList}
